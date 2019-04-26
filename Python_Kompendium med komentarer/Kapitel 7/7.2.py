153ce0970be646ea00cd441a02ddf3c6ff518ef7
abbsimoga <43879969+abbsimoga@users.noreply.github.com>
2019-04-26 12:59:07 +0200

    Update 7.2.py

--- Python_Kompendium med komentarer/Kapitel 7/7.2.py
+++ Python_Kompendium med komentarer/Kapitel 7/7.2.py
@@ -1,17 +1,17 @@
 thenumber = 10 # sätter thenumber till 10
 antalgissningar = 1 # sätter antalet gissningar till 1
 yournumber = thenumber+1 # sätter dittnummer till numrett + 1
-while yournumber != thenumber: # medans ditt 
-    yournumber = int(input("> "))
-    if(yournumber > thenumber):
-        print("Lower")
-        antalgissningar += 1
-        continue
-    elif(yournumber < thenumber):
-        print("Higher")
-        antalgissningar += 1
-        continue
-    print(thenumber)
+while yournumber != thenumber: # medans ditt nummer inte är nummret
+    yournumber = int(input("> ")) # kalla yournumber till det du skriver in i terminalen
+    if(yournumber > thenumber): # om ditt nummer är mer än nummrett
+        print("Lower") # skriv lägre
+        antalgissningar += 1 # lägg till på antalet gissningar
+        continue # fortsätt med whileloopen
+    elif(yournumber < thenumber): # om ditt nummer är mindre än nummrett
+        print("Higher") # skriv högre
+        antalgissningar += 1 # lägg till på antalet gissningar
+        continue # fortsätt med whileloopen
+    print(thenumber) # om du har rätt skriver den nummret är korrejt
     print("is correct")
-    print("it took you " + str(antalgissningar) + " guesses")
-    break
+    print("it took you " + str(antalgissningar) + " guesses") # och hur många gissningar du gjorde
+    break # sedan avslutar den whileloopen
